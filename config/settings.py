c868b2e68b0c0401db712429c20931da97fcc447
Rajeev Swami <rajeevswami056@gmail.com>
2026-07-06 21:26:19 +0530

    Fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4h&ze8_1+!rxx(w3#w@ub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["localhost","127.0.0.1",".onrender.com",]
 
 
 # Application definition
